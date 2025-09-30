d69117d1eb939ce27a482fcdbfd34522766fec9b
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-09-30 17:37:42 +0300

    Department Module [ Entities & Configrations] and DbContext

---
+++ Demo.DataAccess/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Demo.DataAccess.Data.Contexts
+{
+    public class ApplicationDbContext:DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Connection String");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
