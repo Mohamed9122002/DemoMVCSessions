0aa51d9b4a811ff9ed32f94ac753ff22f9884b4e
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-05 15:16:25 +0300

    Install Package and DbContext

--- Demo.DataAccess/Data/Contexts/ApplicationDbContext.cs
+++ Demo.DataAccess/Data/Contexts/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
 ﻿
 
+using Demo.DataAccess.Models.IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 namespace Demo.DataAccess.Data.Contexts
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : DbContext(dbContextOptions) 
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : IdentityDbContext<ApplicationUser>(dbContextOptions) 
     {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
 
         //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
         //{
@@ -16,9 +22,7 @@ namespace Demo.DataAccess.Data.Contexts
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
         }
-
-        public DbSet<Department> Departments { get; set; }
-        public DbSet<Employee> Employees { get; set; } 
-    }
+     }
 }
