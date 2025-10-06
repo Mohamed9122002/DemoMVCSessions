8e62b8616005953905dffd7b63b1f346271fc708
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-06 20:59:17 +0300

    GetAllUser

---
+++ Demo.Presentation/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Demo.DataAccess.Models.IdentityModel;
+using Demo.Presentation.ViewModels.UserViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Buffers;
+
+namespace Demo.Presentation.Controllers
+{
+    public class UserController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager) : Controller
+    {
+        #region GetAll User 
+        [HttpGet] 
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.AsQueryable();
+        
+            var userViewModel = users.Select(u => new UserViewModel()
+            {
+                Id = u.Id,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Email = u.Email,
+            }).ToList();
+            foreach (var user in userViewModel)
+            {
+                user.Roles = _userManager.GetRolesAsync(_userManager.FindByIdAsync(user.Id).Result).Result;
+            }
+            return View(userViewModel);
+        }
+        #endregion
+    }
+}
---
+++ Demo.Presentation/ViewModels/UserViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.Presentation.ViewModels.UserViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public IEnumerable<string> Roles { get; set; } = new List<string>();
+    }
+}
